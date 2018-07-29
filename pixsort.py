7a27c61c31b4648e786207cb73fb2322f98e4697
Bastian Rieck <bastian@rieck.ru>
2018-07-29 17:49:48 +0200

    Added support for command-line arguments

--- pixsort.py
+++ pixsort.py
@@ -3,7 +3,9 @@
 from PIL import Image
 from PIL import ImageFilter
 
+import argparse
 import numpy
+import random
 import sys
 
 def get_row_or_column(image, index, get_row=True):
@@ -82,20 +84,28 @@ def sort(image, edges, index, per_row=True):
   start     = maxima[-1]
 
   data[start:] = sorted(data[start:], reverse=True)
-  image        = set_row_or_column(image, index, data, per_row)
+  image = set_row_or_column(image, index, data, per_row)
   return image
 
-if __name__ == "__main__":
-  image         = Image.open(sys.argv[1])
-  edges         = image.filter(ImageFilter.SHARPEN).filter(ImageFilter.FIND_EDGES)
-  ranges        = calculate_ranges(image)
-  variabilities = calculate_variabilities(image)
-  indices       = numpy.argsort(variabilities)
+if __name__ == '__main__':
 
-  # TODO: make configurable
-  n = 1000
+    parser = argparse.ArgumentParser(description='Applies pixel sorting to an input image')
+    parser.add_argument('input', metavar='INPUT', type=str, help='Input file')
+    parser.add_argument('-o', '--output', type=str, help='Output file')
+    parser.add_argument('-f', '--fraction', type=float, help='What fraction of pixels to distort', default=1.0)
 
-  for i in range(n):
-    image = sort(image, edges, int(indices[i]))
+    arguments = parser.parse_args()
 
-  image.show()
+    image = Image.open(arguments.input)
+    edges = image.filter(ImageFilter.SHARPEN).filter(ImageFilter.FIND_EDGES)
+    ranges = calculate_ranges(image)
+    variabilities = calculate_variabilities(image)
+    indices = numpy.argsort(variabilities)
+
+    n = int(image.size[1] * arguments.fraction)
+
+    for i in range(n):
+        image = sort(image, edges, int(indices[i]))
+
+    if arguments.output:
+        image.save(output)
